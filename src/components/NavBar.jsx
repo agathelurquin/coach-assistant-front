07772aa08585509be9925185ac2a01c33d328dc9
agathelurquin <agathe.lurquin@essca.eu>
2023-09-01 00:40:05 +0200

    responsive but very uggly navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,72 @@
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/AuthContext";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Navbar() {
   const { authenticateUser, isLoggedIn } = useContext(UserContext);
   const navigate = useNavigate();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  useEffect(() => {
+    const closeMobileMenu = () => {
+      setIsMobileMenuOpen(false);
+    };
+    window.addEventListener("resize", closeMobileMenu);
+    return () => {
+      window.removeEventListener("resize", closeMobileMenu);
+    };
+  }, []);
+
   function handleLogout() {
     localStorage.removeItem("authToken");
     authenticateUser();
     navigate("/");
   }
-  console.log("isloggedin", isLoggedIn);
-  return isLoggedIn ? (
-    <div className="loggedin-nav">
-      <Link to="/">
-        <img
-          src="src/assets/img/default_avatar.png"
-          className="logo-navbar"
-          alt="home-logo"
-        />
-      </Link>
-      {/* <Link to="/signup">Signup</Link>
-            <Link to="/login">Log In</Link> */}
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="#">Explore</Link>
-      <Link to="#">My Activity</Link>
 
-      <Link to="/" onClick={handleLogout}>
-        Signout
-      </Link>
-    </div>
-  ) : (
-    <div className="not-loggedin-nav">
-      <Link to="/">
-        <img
-          src="src/assets/img/default_avatar.png"
-          className="logo-navbar"
-          alt="home-logo"
-        />
-      </Link>
-      <Link to="/signup">Signup</Link>
-      <Link to="/login">Log In</Link>
+  return (
+    <div className={`navbar ${isMobileMenuOpen ? "open" : ""}`}>
+      <div className="logo">
+        <Link to="/">
+          <img
+            src="src/assets/img/default_avatar.png"
+            className="logo-navbar"
+            alt="home-logo"
+          />
+        </Link>
+      </div>
+      {isLoggedIn && (
+        <div className={`nav-links ${isMobileMenuOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="#">Explore</Link>
+            </li>
+            <li>
+              <Link to="#">My Activity</Link>
+            </li>
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Signout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+      <button
+        className="navbar-mobile-toggle hidden"
+        onClick={toggleMobileMenu}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <div>
-      <h1>Coach Assistant</h1>
-      <Link to="/signup">
-        <button>Sign Up</button>
-      </Link>
-      <Link to="/login">
-        <button>Log In</button>
-      </Link>
+      <div className="hero-banner">
+        <h1>Coach Assistant</h1>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+        <Link to="/login">
+          <button>Log In</button>
+        </Link>
+      </div>
     </div>
   );
 }
